d0ea0f9f6b2072b2dd628cb077f0f171d8b21106
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-05-08 21:07:16 -0400

    Remove unnecessary styles from command menu

--- src/lib/components/command-menu.svelte
+++ src/lib/components/command-menu.svelte
@@ -44,7 +44,7 @@
   </Dialog.Trigger>
   <Dialog.Content
     showCloseButton={false}
-    class="rounded-xl border-none bg-clip-padding p-2 pb-11 shadow-2xl ring-4 ring-neutral-200/80 dark:bg-neutral-900 dark:ring-neutral-800"
+    class="bg-clip-padding p-2 pb-11"
   >
     <Dialog.Header class="sr-only">
       <Dialog.Title>Search components...</Dialog.Title>
@@ -79,7 +79,7 @@
       </Command.List>
     </Command.Root>
     <div
-      class="absolute inset-x-0 bottom-0 z-20 flex h-10 items-center gap-2 rounded-b-xl border-t border-t-neutral-100 bg-neutral-50 px-4 text-xs font-medium text-muted-foreground dark:border-t-neutral-700 dark:bg-neutral-800"
+      class="absolute inset-x-0 bottom-0 z-20 flex h-10 items-center gap-2 rounded-b-xl border-t px-4 text-xs font-medium text-muted-foreground"
     >
       <div class="flex items-center gap-2">
         <div
